e1d569a6f40ffcae5f77f7ad0815ca19bf913b62
Raj Wilkhu <rajwilkhu@gmail.com>
2017-05-08 21:28:21 +0100

    Run npm install for dev dependencies

--- commands/installCommand.js
+++ commands/installCommand.js
@@ -5,7 +5,7 @@ module.exports = (serviceDirectory, installDevDependencies) => {
   util.log(`Executing npm install in directory ${serviceDirectory}, install dev dependencies = ${installDevDependencies ? 'true' : 'false'}`)
   return new Promise((resolve, reject) => {
     const intallScript = installDevDependencies
-      ? 'npm install --ignore-scripts'
+      ? 'npm install'
       : 'npm install --production --ignore-scripts';
     exec(intallScript, { cwd: serviceDirectory }, (error, stdout, stderr) => {
       if (error) {
